6d5a4383e43d7656c3c9173fe67b1c8e0d24a269
Kirill Sagorin <st128530@student.spbu.ru>
2025-11-12 02:25:04 +0300

    added time calculation

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,28 +1,53 @@
 #include "ClassBMP.h"
+#include <chrono>
+#include <iostream>
 
 int main()
 {
     try
     {
+        auto total_start = std::chrono::high_resolution_clock::now();
         // Load the image file
         ClassBMP image_one("a.bmp");   
 
         // Perform a clockwise rotation
         std::cout << "Rotating image clockwise by 90 degrees..." << std::endl;
+        auto start = std::chrono::high_resolution_clock::now();
         image_one.RotateClockwise90();      
+        auto end = std::chrono::high_resolution_clock::now();
         image_one.SaveToFile("Rotated90Clockwise.bmp");
+        std::cout << "Right rotation time: "
+                  << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+                  << " ms" << std::endl;
 
         ClassBMP image_two("a.bmp");  
         // Perform a counter-clockwise rotation
         std::cout << "Rotating image counter-clockwise by 90 degrees..." << std::endl;
+        start = std::chrono::high_resolution_clock::now();
         image_two.RotateCounterClockwise90();
+        end = std::chrono::high_resolution_clock::now();
         image_two.SaveToFile("Rotated90CounterClockwise.bmp");
+        std::cout << "Left rotation time: "
+                  << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+                  << " ms" << std::endl;
 
         ClassBMP image_three("a.bmp");  
         // Apply Gaussian Blur effect
         std::cout << "Applying Gaussian Blur to the image..." << std::endl;
+        start = std::chrono::high_resolution_clock::now();
         image_three.ApplyGaussianBlur();
+        end = std::chrono::high_resolution_clock::now();
         image_three.SaveToFile("GaussianBlur.bmp");
+        std::cout << "Writing filtering time: "
+                  << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+                  << " ms" << std::endl;
+
+        auto total_end = std::chrono::high_resolution_clock::now();
+        image_three.SaveToFile("GaussianBlur.bmp");
+        std::cout << "All time taken: "
+                  << std::chrono::duration_cast<std::chrono::milliseconds>(total_end - total_start).count()
+                  << " ms" << std::endl;
+
 
         std::cout << "Image processing completed successfully!" << std::endl;
     }
@@ -34,3 +59,4 @@ int main()
 
     return 0;
 }
+
